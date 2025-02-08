3ab2404e012b3639df834e29a550925052edc99e
Udonne Chinonso <nonso.udonne@gmail.com>
2025-02-09 00:09:02 +0100

    qa ready

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express"
+import cors from "cors"
+import { armstrong_checker, fetch_fact, perfect_checker, prime_checker } from "./utils.js";
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(cors())
+app.use(express.json());
+
+const apiPrefix = "/api"
+
+
+app.get(`${apiPrefix}/ping`, (req, res) => {
+    res.status(200).json({
+        "status": "live",
+        "response": "pong"
+    })
+})
+
+app.get(`${apiPrefix}/classify-number`, async (req, res) => {
+    let { number } = req.query;
+    
+    
+    if (!number || isNaN(number)){
+        res.status(400).json({
+            "number": "alphabet",
+            "error": true
+        });
+    }
+    
+    number = parseInt(number);
+    const properties = []
+
+    const is_prime = prime_checker(number);
+    const is_perfect = perfect_checker(number);
+
+    armstrong_checker(number) === true && properties.push("armstrong")
+    number % 2 === 0 ? properties.push("even") : properties.push("odd")
+
+    const digit_sum = number.toString().split('').reduce((acc, num) => acc + parseInt(num), 0)
+    const fun_fact = await fetch_fact(number);
+
+    res.json({
+        "number": number,
+        "is_prime": is_prime,
+        "is_perfect": is_perfect,
+        "properties": properties,
+        "digit_sum": digit_sum,
+        "fun_fact": fun_fact
+    })
+})
+
+app.all("*", (req, res) => {
+    res.status(404).json({
+        "error": `Can't find resource "${req.originalUrl}" on this server`
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`)
+})
+
+export default app
---
+++ utils.js
@@ -0,0 +1,47 @@
+export const prime_checker = (number) => {
+    let isPrime = number > 1 && !(number % 2 === 0 && number !== 2);
+
+    for (let i = 3; i * i <= number && isPrime; i += 2) {
+        if (number % i === 0) {
+            isPrime = false;
+        }
+    }
+
+    return isPrime
+}
+
+export const perfect_checker = (number) => {
+    let temp = 0;
+    
+    for (let i = 1; i <= number / 2; i++) {
+        if (number % i === 0) {
+            temp += i;
+        }
+    }
+
+    return temp === number && temp !== 0
+}
+
+export const armstrong_checker = (number) => {
+    const digits = number.toString().split('');
+    const order = digits.length;
+    const sum = digits.reduce((acc, digit) => acc + Math.pow(parseInt(digit), order), 0);
+
+    return sum === number
+}
+
+export const fetch_fact = async (number) => {
+    const url = `http://numbersapi.com/${number}/math?json`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        return json.text;
+    } catch (error) {
+        console.error('message', error.message);
+        return error.message;
+    }
+}
